d2ee5de0b65166643cd457bec3aa5b1eaf671465
pratibha-09-ai <prati09bha@gmail.com>
2026-06-11 19:55:06 +0530

    fix: remove duplicate Post. find One query and broken merge artifact
    Fixed duplicate Post.findOne query in reaction.service.ts.
    
    Removed broken merge artifact and invalid standalone statements that were causing syntax issues in toggleReaction.
    
    Ensured the reaction flow remains intact for adding, updating, and removing reactions.

--- backend/src/app/modules/reaction/reaction.service.ts
+++ backend/src/app/modules/reaction/reaction.service.ts
@@ -1,3 +1,4 @@
+```ts
 import ApiError from "../../../errors/api_error";
 import { ITokenPayload } from "../../../interfaces/token";
 import { User } from "../user/user.model";
@@ -21,9 +22,6 @@ const toggleReaction = async (
   }
 
   const post = await Post.findOne({
-  _id: postId,
-  isDeleted: { $ne: true },
-}).select("likesCount reactions");
     _id: postId,
     isDeleted: { $ne: true },
   }).select("likesCount reactions");
@@ -32,9 +30,6 @@ const toggleReaction = async (
     throw new ApiError(httpStatus.BAD_REQUEST, "Post not found!");
   }
 
-//  main
-    const newReaction = await Reaction.create({
-      postId: new Types.ObjectId(postId),
   const existingReaction = await Reaction.findOne({
     postId: post._id,
     userId: user._id,
@@ -44,9 +39,11 @@ const toggleReaction = async (
     if (existingReaction.type === type) {
       // Remove reaction if the same type is toggled
       await Reaction.findByIdAndDelete(existingReaction._id);
+
       post.reactions = (post.reactions || []).filter(
         (id) => id && id.toString() !== existingReaction._id.toString()
       );
+
       post.likesCount = Math.max(0, (post.likesCount || 0) - 1);
       await post.save();
 
@@ -69,10 +66,12 @@ const toggleReaction = async (
     const newReaction = await Reaction.create({
       postId: post._id,
       userId: user._id,
-      type: type,
+      type,
     });
+
     post.reactions = post.reactions || [];
     post.reactions.push(newReaction._id);
+
     post.likesCount = (post.likesCount || 0) + 1;
     await post.save();
 
@@ -86,3 +85,4 @@ const toggleReaction = async (
 export const ReactionService = {
   toggleReaction,
 };
+```
